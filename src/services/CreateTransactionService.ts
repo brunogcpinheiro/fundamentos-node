2b63b42f6fa570cb8faa1498c932227961912b16
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2020-05-22 12:46:23 +0000

    finish challenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  private hasBalance(outcomeValue: number): boolean {
+    const { total } = this.transactionsRepository.getBalance();
+
+    return total <= outcomeValue;
+  }
+
+  public execute({ title, type, value }: Request): Transaction {
+    if (type === 'outcome' && this.hasBalance(value)) {
+      throw Error('No balance to debit this outcome!');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
